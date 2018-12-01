e8c6ca496e3894767e7bc23f675159d029f95415
Rui <905153643@qq.com>
2018-12-01 15:49:58 +0800

    debug

--- src/main/webServer/routes.js
+++ src/main/webServer/routes.js
@@ -2,10 +2,8 @@ const express = require("express"),
 	router = express.Router();
 
 //GET home page.
+
 router.get('/getData', function (req, res, next) {
 	res.send("data")
 });
-// router.post('/upload',function (req,res,nex) {
-// 	res.send("data")
-// })
 module.exports = router;
