be2643898dfc963c1e347840e2d6af7cc9d48eba
Rui <905153643@qq.com>
2018-12-06 22:45:31 +0800

    提交

--- src/main/webServer/routes.js
+++ src/main/webServer/routes.js
@@ -5,13 +5,10 @@ const express = require("express"),
 
 router.get('/getData', function (req, res, next) {
 	let classToJobDb = new global.ClassToJobDb();
-	let cursor = classDb.findByStatus(1);
-	console.log(cursor);
+	let cursor = classToJobDb.findByStatus(1);
 	cursor.exec((error,docs) =>{
 		res.send(docs);
 	});
-	cursor.exec((error, docs) => {
-	})
 	
 });
 module.exports = router;
