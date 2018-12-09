4747346dcff920db402d8d20e530ccc61636968f
EchoCow <lizhongyue248@163.com>
2018-12-09 22:44:22 +0800

    :zap:动态生成左侧菜单栏

--- src/renderer/components/SideBar.vue
+++ src/renderer/components/SideBar.vue
@@ -1,16 +1,27 @@
 <template lang="pug">
-  el-col(:span="18" id="sideBar")
-    el-menu(router active-text-color="#ffffff")
-      el-menu-item(index="/")
-        i.el-icon-menu &nbsp;&nbsp;主页
-          <!--el-badge.mark(:value="12" type="primary" :max="10" )-->
-      el-menu-item(index="/class")
-        i.el-icon-edit &nbsp;&nbsp;班级管理
-      el-menu-item(index="/job")
-        i.el-icon-question &nbsp;&nbsp;作业管理
-      el-menu-item(index="/server")
-        i.el-icon-question &nbsp;&nbsp;作业收取
+  el-col(:span="18", id="sideBar")
+    el-menu(router, active-text-color="#ffffff")
+      template(v-for="(nav, index) in navList")
+        el-menu-item(:index="nav.name")
+          i(:class="nav.icon")
+          | &nbsp;&nbsp;{{nav.navItem}}
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        navList: [
+          {name: '/', navItem: '主页', icon: 'el-icon-menu'},
+          {name: '/class', navItem: '班级管理', icon: 'el-icon-edit'},
+          {name: '/job', navItem: '作业管理', icon: 'el-icon-setting'},
+          {name: '/server', navItem: '作业收取', icon: 'el-icon-upload2'},
+          {name: '/monitor', navItem: '实时监控', icon: 'el-icon-loading'}
+        ]
+      };
+    }
+  }
+</script>
 
 <style lang="stylus">
   @import "../styles/components/SideBar.styl"
