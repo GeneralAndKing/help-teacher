a3170cf5903a979a2bf9c646a4973505442c3bcb
EchoCow <lizhongyue248@163.com>
2018-12-09 21:53:31 +0800

    :lipstick:优化开启服务页面

--- src/renderer/pages/server/index.vue
+++ src/renderer/pages/server/index.vue
@@ -7,42 +7,48 @@
     #gak-main-server
       el-container
         el-header
-          el-steps(:active="active", align-center, finish-status='success')
+          el-steps(:active="active", align-center, finish-status="success")
             el-step(title="开始作业")
-            el-step(title="设置作业")
+            el-step(title="设置班级")
             el-step(title="开启服务")
         el-main.basic
-          el-form(:model="server" label-width="80px" label-position="left")
+          el-form(:model="form" label-width="80px" label-position="left")
             transition(name="slide-fade", mode="out-in")
-              .basicIndex(v-if="active===0", :key="0")
+              .basicStart(v-if="active===0", :key="0")
+                el-alert.gak-text-left(title="开始使用", type="success", description="您只需要简单的散三步即可开启本次作业的收取！现在，请选择您要开启的作业。")
                 el-form-item.gak-text-left(label="作业名称")
-                  el-select(v-model='selectJob', placeholder='请选择')
-                    el-option(v-for='job in jobs', :key='job.jobName', :label='job.jobName', :value='job.jobName')
+                  el-select(v-model="form.jobName", placeholder="请选择")
+                    el-option(v-for="job in jobs", :key="job.jobName", :label="job.jobName", :value="job.jobName")
                   span.gak-text-placeholder 没有作业？
-                    a#gak-job-create(@click="createJob") 点击创建
+                    a.gak-job-create(@click="createJob") 点击创建
                 el-form-item(label="时间(分钟)")
-                  el-slider(v-model="server.time", :min="10", :max="60", label="提交时间", show-input)
+                  el-slider(v-model="form.stopTime", :min="1", :max="120", label="提交时间", show-input)
               .basicData(v-if="active===1", :key="1")
+                el-alert.gak-text-left(title="设置班级", type="success", description="接下来，请选择您要开启的班级。")
+                el-form-item.gak-text-left(label="作业名称")
+                  el-select(v-model="form.className", placeholder="请选择")
+                    el-option(v-for="classDb in classes", :key="classDb.className", :label="classDb.className", :value="classDb.className")
+                  span.gak-text-placeholder 没有班级？
+                    a.gak-job-create(@click="createClass") 点击创建
               .basicServer(v-if="active===2", :key="2")
-                el-button(@click="openServer" type="primary") 开启服务
+                el-alert.gak-text-left(title="最后一步", type="success", description="最后，您只需要设置学生端的端口就完成了。")
+                el-form-item(label="端口号")
+                  el-input-number.gak-text-left(v-model='form.port', :step='50')
+
         el-footer
           el-button-group
             transition(name="el-fade-in", mode="out-in")
-              el-button(type="primary" icon="el-icon-arrow-left" v-if="active!==0" @click="prev") 上一步
+              el-button(type="primary", icon="el-icon-arrow-left", v-if="active!==0", @click="prev") 上一步
             transition(name="el-fade-in", mode="out-in")
-              el-button(type="primary" icon="el-icon-arrow-right" v-if="active!==2" @click="next") 下一步
+              el-button(type="primary", icon="el-icon-arrow-right", v-if="active!==2", @click="next") 下一步
+              el-button(@click="openServer", type="primary", v-if="active===2") 开启服务
 
 </template>
-<style lang="stylus" scoped>
-  @import "../../styles/server/index.styl"
-
-</style>
-
 
 <script>
   import {Loading} from "element-ui";
   import {runInNewContext} from "vm";
-  import {clearInterval} from 'timers';
+  import {clearInterval} from "timers";
 
   const {ipcRenderer} = require("electron");
   const remote = require("electron").remote;
@@ -57,11 +63,14 @@
     data() {
       return {
         active: 0,
-        server: {
-          title: "",
-          time: 0
+        form: {
+          className: "",
+          jobName: "",
+          startTime: "",
+          stopTime: "",
+          port: 8888,
+          status: 1
         },
-        selectJob: "",
         jobs: [
           {
             jobName: "第一次作业",
@@ -91,6 +100,51 @@
               }
             ]
           }
+        ],
+        classes: [
+          {
+            className: "2016级计算机科学与技术2班",
+            students: [
+              {
+                id: "201607010244",
+                name: "睿哥",
+                sex: "男"
+              },
+              {
+                id: "201607010244",
+                name: "睿少",
+                sex: "男"
+              }
+            ]
+          }, {
+            className: "2016级计算机科学与技术1班",
+            students: [
+              {
+                id: "201607010244",
+                name: "睿哥",
+                sex: "男"
+              },
+              {
+                id: "201607010244",
+                name: "睿少",
+                sex: "男"
+              }
+            ]
+          }, {
+            className: "2016级计算机科学与技术3班",
+            students: [
+              {
+                id: "201607010244",
+                name: "睿哥",
+                sex: "男"
+              },
+              {
+                id: "201607010244",
+                name: "睿少",
+                sex: "男"
+              }
+            ]
+          }
         ]
       };
     },
@@ -113,9 +167,20 @@
       },
       createJob: function () {
         this.$router.push({
-          name: 'job'
+          name: "job"
+        });
+      },
+      createClass: function () {
+        this.$router.push({
+          name: "class"
         });
       }
     }
   };
 </script>
+
+
+<style lang="stylus" scoped>
+  @import "../../styles/server/index.styl"
+
+</style>
