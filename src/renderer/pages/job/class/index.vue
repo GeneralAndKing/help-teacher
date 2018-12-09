d9a4317c5c2477ceff1d583c2000dde83b31e82f
EchoCow <lizhongyue248@163.com>
2018-12-09 15:04:41 +0800

    作业——班级页面完成

--- src/renderer/pages/job/class/index.vue
+++ src/renderer/pages/job/class/index.vue
@@ -5,11 +5,11 @@
       i.el-icon-more#gak-main-head-nav(@click="$emit('changeSide')")
       span#gak-main-head-title Class
     el-scrollbar#gak-main-content-class
-      el-table(:data='classToJobs', height="250", v-loading="loading")
+      el-table(:data='classToJobs.filter(data => !search || data.className.toLowerCase().includes(search.toLowerCase()))', stripe,  v-loading="loading")
         el-table-column(type='index', width='40', align="center")
-        el-table-column(label='班级', prop="className", width="155px", align="center")
+        el-table-column(label='班级', prop="className", align="center")
         el-table-column(label='人数', prop='studentNum', width="50px", align="center")
-        el-table-column(label='开始时间', prop='startTime', width="100px", sortable, align="center")
+        el-table-column(label='开始时间', prop='startTime',  sortable, align="center")
         el-table-column(label='状态', prop='status', width="80px", sortable, align="center")
           template(slot-scope='scope')
             el-tag(type='info', v-if="scope.row.status === 0") 未开启
@@ -17,9 +17,9 @@
             el-tag(type='success', v-if='scope.row.status === 2') 已完成
         el-table-column(align="center")
           template(slot='header', slot-scope='scope')
-            el-input(v-model='search', size='mini', placeholder='输入学号或姓名搜索', style="width:80%;max-width:200px;float:right;", clearable)
+            el-input(v-model='search', size='mini', placeholder='输入班级搜索', style="max-width:250px;float:right;", clearable)
           template(slot-scope='scope')
-            el-button(size='mini', @click='handleSave($event,scope.$index, scope.row)' ) 详情
+            el-button(size='mini', @click='handleInfo($event,scope.$index, scope.row)' ) 详情
             el-button(size='mini', type='danger', @click='handleDelete(scope.$index, scope.row)' ) 删除
 
 </template>
@@ -36,9 +36,153 @@
         loading: true,
         search: '',
         classToJobs: [{
+          className: "2016软件工程",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016啊萨斯的",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计浮动浮动术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
           className: "2016计算机科学与技术",
           jobName: "作业名字",
-          startTime: "",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 0,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 1,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
+          stopTime: 30,
+          studentNum: 68,
+          status: 2,
+          unfinishedPeoples: [{
+            name: "樊总",
+            id: "201607010244",
+            sex: "男"
+          }, {
+            name: "睿总",
+            id: "201607010244",
+            sex: "男"
+          }]
+        },{
+          className: "2016计算机科学与技术",
+          jobName: "作业名字",
+          startTime: "2018年12月12日",
           stopTime: 30,
           studentNum: 68,
           status: 2,
@@ -75,6 +219,9 @@
         // let ClassDb = remote.getGlobal("ClassDb");
         // let classDb = new ClassDb();
         success(_this, "删除成功");
+      },
+      handleInfo(index, row){
+
       }
     }
   };
