a07aac1613a26e0d5b8a6a32c8f8f15232554766
EchoCow <lizhongyue248@163.com>
2018-12-15 01:20:59 +0800

    :hammer: web 重大修复，页面重构

--- src/web/App.vue
+++ src/web/App.vue
@@ -1,34 +1,34 @@
 <template lang="pug">
-    div#app
-        router-view
+  #app
+    router-view
 </template>
 
 <style lang="stylus">
-    /* stylus */
+  /* stylus */
 </style>
 
 
 <script>
-    export default {
-        name: 'help-teacher',
-        provide(){
-            return {
-                reload: this.reload
-            }
-        },
-        data(){
-            return {
-                isRouterAlive: true
-            }
-        },
-        methods:{
-            reload(){
-                this.isRouterAlive = false;
-                this.$nextTick(function(){
-                    this.isRouterAlive = true
-                })
-            }
-        }
+  export default {
+    name: 'help-teacher',
+    provide() {
+      return {
+        reload: this.reload
+      }
+    },
+    data() {
+      return {
+        isRouterAlive: true
+      }
+    },
+    methods: {
+      reload() {
+        this.isRouterAlive = false;
+        this.$nextTick(function () {
+          this.isRouterAlive = true
+        })
+      }
     }
+  }
 
 </script>
--- src/web/pages/home/index.vue
+++ src/web/pages/home/index.vue
@@ -1,230 +1,245 @@
 <template lang="pug">
-    el-container(class='container')
-        el-header(class='header') Help-Teacher
-        el-container
-            el-aside(width='50%')
-                #app(width='100%')
-                    #gak-main-chart(style='width: 600px;height:400px;')
-
-            el-main(class='main',width='50%') 交作业
-                #title 作业标题:{{jobName}}
-                #content 作业详情:{{jobContent}}
-                el-form(ref='form', :model='form', label-width='200px')
-                    el-form-item(label='学号',prop='StudentId',:rules="[\
-          { required: true, message: '学号不能为空'},\
-          { type: 'number', message: '学号必须为数字值'}\
+  el-container(class='container')
+    el-header(class='header')
+      #el-header-title Help-Teacher
+      img#el-header-logo(src='../../assets/logo.png')
+    el-container
+      el-main(class='main', style="max-width:800px;")
+        .gak-tip-blue
+          #gak-main-name 作业收取
+          #gak-main-title 作业标题：{{jobName}}
+          #gak-main-content 作业详情：{{jobContent}}
+          #gak-main-time 剩余时间：{{jobContent}}
+        el-form(:model='form', ref="form", :rules='rules')
+          el-form-item(label='学号', prop='StudentId', :rules="[\
+            { required: true, message: '学号不能为空'}\
           ]")
-                        el-input(type='StudentId',v-model.number='form.StudentId')
-                    el-form-item(label='作业文件')
-                        el-upload.upload-demo(ref='upload', action='/upload', :on-preview='handlePreview',:on-success="submitForm",:on-error="handleError",:on-exceed="handleExceed",:before-remove="beforeRemove" ,:on-change='handleChange',:on-remove='handleRemove', :file-list='fileList',:auto-upload='false',:limit=1,accept='{{ fileTypes }}')
-                            el-button(slot='trigger', size='small', type='primary') 选取文件
-                            .el-upload__tip(slot='tip') 只能上传格式为{{fileTypes}}的文件。
-                    el-form-item
-                        el-button(type='primary', @click='onSubmit(\'form\')') 提交作业
-                        el-button(@click="resetForm(\'form\')") 重置
+            el-input(type="text", v-model='form.StudentId', autocomplete='off', clearable, placeholder="请输入学号")
+          el-form-item(label='作业文件',  prop='input')
+            el-upload.upload-demo(ref='upload', action='/upload', :on-preview='handlePreview', :on-success="submitForm",
+            :on-error="handleError", :on-exceed="handleExceed", :on-change='handleChange',
+            :on-remove='handleRemove', :file-list='fileList', :auto-upload='false', :limit=1, :before-upload="beforeAvatarUpload")
+              el-button(slot='trigger', size='small', type='primary') 选取文件
+              .el-upload__tip(slot='tip') 只能上传格式为{{ fileTypes }}的文件。
+          el-form-item(style="text-align:center;")
+            el-button(type='primary', @click='onSubmit(\'form\')') 提交作业
+            el-button(@click="resetForm(\'form\')") 重置
+        #app(width='100%')
+          #gak-main-chart(style='width: 100%;height:400px;')
 </template>
-<style lang="stylus">
-    @import '../../styles/home/index.styl';
-</style>
+
 <script>
-    export default {
-        inject: ['reload'],
-        data() {
-            return {
-                fileList: [],
-                form: {
-                    StudentId: ''
-                },
-                fileCount: 0,
-                fileUpload: false,
-                jobName: '',
-                jobContent: '',
-                fileTypes:[],
-                data: [{ value: 0, name: "未交人数" }, { value: 0, name: "已交人数" }],
-                option: {
-                    title: {
-                        text: "",
-                        x: "center"
-                    },
-                    tooltip: {
-                        trigger: "item",
-                        formatter: "{b} : {c} ({d}%)"
-                    },
-                    legend: {
-                        orient: "vertical",
-                        left: "left",
-                        data: ["已交人数", "未交人数"]
-                    },
-                    color: ["#F66", "#9CF"],
-                    series: [
-                        {
-                            name: "作业情况",
-                            type: "pie",
-                            radius: "80%",
-                            center: ["50%", "55%"],
-                            data: [],
-                            itemStyle: {
-                                emphasis: {
-                                    shadowBlur: 10,
-                                    shadowOffsetX: 0,
-                                    shadowColor: "rgba(0, 0, 0, 0.5)"
-                                }
-                            }
-                        }
-                    ]
+  export default {
+    inject: ['reload'],
+    data() {
+      return {
+        fileList: [],
+        form: {
+          StudentId: ''
+        },
+        fileCount: 0,
+        fileUpload: false,
+        jobName: '',
+        jobContent: '',
+        leaveTime: '',
+        fileTypes: "",
+        data: [{value: 0, name: "未交人数"}, {value: 0, name: "已交人数"}],
+        option: {
+          title: {
+            text: "",
+            x: "center"
+          },
+          tooltip: {
+            trigger: "item",
+            formatter: "{b} : {c} ({d}%)"
+          },
+          legend: {
+            orient: "vertical",
+            left: "left",
+            data: ["已交人数", "未交人数"]
+          },
+          color: ["#F66", "#9CF"],
+          series: [
+            {
+              name: "作业情况",
+              type: "pie",
+              radius: "80%",
+              center: ["50%", "55%"],
+              data: [],
+              itemStyle: {
+                emphasis: {
+                  shadowBlur: 10,
+                  shadowOffsetX: 0,
+                  shadowColor: "rgba(0, 0, 0, 0.5)"
                 }
+              }
             }
-        },
-        methods: {
-            onSubmit(formName) {
-                let _this = this;
-                this.$refs[formName].validate((valid) => {
-                    if (valid) {
-                        let form = _this.$refs['form'].$el;
-                        console.log(_this.fileList.length);
-                        if(_this.fileCount != 1 || this.fileUpload){
-                            if(this.fileUpload){
-                                _this.$message.warning('已经上传过了，重新选择文件再次尝试上传！！');
-                            }else{
-                                _this.$message.warning('必须选择一个文件才能提交！');
-                            }
-                        }else{
-                            //执行表单提交以及文件上传
-                            _this.$refs.upload.submit();
-                        }
-                    } else {
-                        console.log('error submit!!');
-                        _this.$message.warning('作业无法提交请检查输入是否完整！');
-                        return false;
-                    }
-                });
-            },
-            handleRemove(file, fileList) {
-                // console.log(file, fileList);
-                this.fileCount = 0;
-                // console.log(this.fileCount);
-            },
-            handlePreview(file) {
-                console.log(file);
-            },
-            handleExceed(files, fileList) {
-                this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
-            },
-            beforeRemove(file, fileList) {
-                return this.$confirm(`确定移除 ${ file.name }？`);
-            },
-            handleChange(file, fileList){
-                if(file){
-                    this.fileCount = 1;
-                }
-                this.fileUpload = false;
-            },
-            submitForm(response, file, fileList){
-                //文件上传成功以后提交表单
-                let formData = new FormData();
-                let filename = file.response.data.name;
-                //文件扩展名
-                let arr = filename.split('.');
-                let type = arr[arr.length-1];
-                let fileTempPath = file.response.data.path;
-                //封装数据
-                formData.append('fileType',type);
-                formData.append('fileTempPath',fileTempPath);
-                formData.append('StudentId',this.form.StudentId);
-                this.$http.post('/submitHomework',formData)
-                    .then(response => {
-                        if(response.status == 200){
-                            console.log("成功");
-                            this.fileUpload = true;
-                            if(response.data.status == 1){
-                                this.$message.success('作业提交成功，重复提交相同文件会覆盖上一次提交的文件！');
-                                this.reload();
-                            }else{
-                                this.$message.error('作业提交失败'+ response.data.error +'！');
-                            }
-                            
-                        }
-                    })
-                    .catch(function(error) {
-                        console.log(error)
-                        this.fileUpload = false;
-                        this.$message.error('错了哦，无法访问接口！');
-                    });
-            },
-            handleError(err, file, fileList){
-                console.log(err);
-                this.fileCount = 0;
-                this.fileUpload = false;
-                this.$message.error('错了哦，无法上传文件！');
-            },
-            resetForm(formName) {
-                this.$refs[formName].resetFields();
-            },
-            handleCurrentChange(cpage) {
-                this.currpage = cpage;
-            },
-            handleSizeChange(psize) {
-                this.pagesize = psize;
-            },
-            getInfo(){
-                let _this = this;
-                    // 获取未交学生列表
-                    this.$http.get('/getJobInformation')
-                        .then(response => {
-                            if(response.status == 200){
-                                console.log("成功访问数据接口");
-                                console.log(response.data);
-                                if(response.data){
-                                    _this.jobName = response.data.data.jobName;
-                                    _this.jobContent = response.data.data.jobContent;
-                                    _this.data[0].value = response.data.data.unfinishedStudentNum;
-                                    _this.data[1].value = response.data.data.studentNum;
-                                    types = response.data.data.jobTypes;
-                                    typeList = [];
-                                    //遍历文件列表 限制文件类型
-                                    for(const type of types){
-                                        console.log(type);
-                                        if(type == 'zip'){
-                                            typeList.push("application/zip");
-                                        }else if(type == 'rar'){
-                                            typeList.push("application/x-rar");
-                                        }else if(type =='ppt'){
-                                            typeList.push("application/vnd.ms-powerpoint");
-                                            typeList.push("application/vnd.openxmlformats-officedocument.presentationml.presentation");
-                                        }else if(type == 'xls'){
-                                            typeList.push("application/vnd.ms-excel");
-                                            typeList.push("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-                                        }else if(type == 'word'){
-                                            typeList.push("application/msword");
-                                            typeList.push("application/vnd.openxmlformats-officedocument.wordprocessingml.document");
-                                        }
-                                    }
-                                    _this.fileTypes = typeList.join();
-                                }
-                            }
-                        })
-                        .catch( error => {
-                            console.log(error);
-                        });
-                    
-            }
-        },
-        created() {
-            // let _this = this;
-            // _this.classToJob = _this.$route.params.classToJob;
-            // _this.data[0].value = _this.classToJob.unfinishedStudents.length;
-            // _this.data[1].value = _this.classToJob.studentNum - _this.data[0].value;
-        },
-        mounted() {
-            this.getInfo();
-            let _this = this;
-            let myChart = _this.$echarts.init(
-                document.getElementById("gak-main-chart")
-            );
-            //设置图表option
-            _this.option.series[0].data = _this.data;
-            myChart.setOption(_this.option);
+          ]
         }
+      }
+    },
+    methods: {
+      onSubmit(formName) {
+        let _this = this;
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            let form = _this.$refs['form'].$el;
+            console.log(_this.fileList.length);
+            if (_this.fileCount != 1 || _this.fileUpload) {
+              if (_this.fileUpload) {
+                _this.$message.warning('已经上传过了，重新选择文件再次尝试上传！！');
+              } else {
+                _this.$message.warning('必须选择一个文件才能提交！');
+              }
+            } else {
+              //执行表单提交以及文件上传
+              _this.$refs.upload.submit();
+            }
+          } else {
+            console.log('error submit!!');
+            _this.$message.warning('作业无法提交请检查输入是否完整！');
+            _this.fileUpload = false;
+            return false;
+          }
+        });
+      },
+      handleRemove(file, fileList) {
+        // console.log(file, fileList);
+        this.fileCount = 0;
+        // console.log(this.fileCount);
+      },
+      handlePreview(file) {
+        console.log(file);
+      },
+      handleExceed(files, fileList) {
+        this.$message.warning(`当前限制选择 1 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+      },
+      beforeRemove(file, fileList) {
+        return this.$confirm(`确定移除 ${file.name}？`);
+      },
+      handleChange(file, fileList) {
+        if (file) {
+          this.fileCount = 1;
+        }
+        this.fileUpload = false;
+      },
+      submitForm(response, file, fileList) {
+        let _this = this;
+        //文件上传成功以后提交表单
+        let formData = new FormData();
+        let filename = file.response.data.name;
+        //文件扩展名
+        let arr = filename.split('.');
+        let type = arr[arr.length - 1];
+        let fileTempPath = file.response.data.path;
+        //封装数据
+        formData.append('fileType', type);
+        formData.append('fileTempPath', fileTempPath);
+        formData.append('StudentId', this.form.StudentId);
+        this.$http.post('/submitHomework', formData)
+          .then(response => {
+            if (response.status == 200) {
+              console.log("成功");
+              if (response.data.status == 1) {
+                _this.$message.success('作业提交成功，重复提交相同文件会覆盖上一次提交的文件！');
+                _this.fileUpload = true;
+                _this.reload();
+              } else {
+                _this.fileUpload = false;
+                _this.$message.error('作业提交失败,' + response.data.error + '！');
+                _this.$refs.upload.clearFiles();
+              }
+            }
+          })
+          .catch(function (error) {
+            console.log(error)
+            this.fileUpload = false;
+            this.$message.error('错了哦，无法访问接口！');
+          });
+      },
+      handleError(err, file, fileList) {
+        this.fileCount = 0;
+        this.fileUpload = false;
+        this.$message.error('错了哦，无法上传文件！');
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      },
+      handleCurrentChange(cpage) {
+        this.currpage = cpage;
+      },
+      handleSizeChange(psize) {
+        this.pagesize = psize;
+      },
+      getInfo() {
+        let _this = this;
+        // 获取未交学生列表
+        this.$http.get('/getJobInformation')
+          .then(response => {
+            if (response.status == 200) {
+              console.log("成功访问数据接口");
+              console.log(response.data);
+              if (response.data) {
+                _this.jobName = response.data.data.jobName;
+                _this.jobContent = response.data.data.jobContent;
+                _this.data[0].value = response.data.data.unfinishedStudentNum;
+                _this.data[1].value = response.data.data.studentNum;
+                let types = response.data.data.jobTypes.split(",");
+                let typeList = [];
+                //遍历文件列表 限制文件类型
+                for (const type of types) {
+                  if (type == '.excel') {
+                    typeList.push(".xlsx");
+                    typeList.push(".xls");
+                  } else if (type == '.word') {
+                    typeList.push(".doc");
+                    typeList.push(".docx");
+                  } else if (type == '.ppt') {
+                    typeList.push(".ppt");
+                    typeList.push(".pptx");
+                  } else {
+                    typeList.push(type);
+                  }
+                }
+                _this.fileTypes = typeList.join(",");
+                console.log(_this.fileTypes);
+              }
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      },
+      beforeAvatarUpload: function (file) {
+        let _this = this;
+        let type = file.name.substring(file.name.lastIndexOf('.') + 1);
+        let res = _this.fileTypes.indexOf(type) > 0;
+        if (!res){
+          this.$message.error('上传文件格式不合法，只能为' + _this.fileTypes + '格式。');
+        }
+        return res;
+      }
+    },
+    created() {
+      // let _this = this;
+      // _this.classToJob = _this.$route.params.classToJob;
+      // _this.data[0].value = _this.classToJob.unfinishedStudents.length;
+      // _this.data[1].value = _this.classToJob.studentNum - _this.data[0].value;
+    },
+    mounted() {
+      this.getInfo();
+      let _this = this;
+      let myChart = _this.$echarts.init(
+        document.getElementById("gak-main-chart")
+      );
+      //设置图表option
+      _this.option.series[0].data = _this.data;
+      myChart.setOption(_this.option);
     }
+  }
 
 </script>
+
+<style lang="stylus">
+  @import '../../styles/home/index.styl';
+</style>
