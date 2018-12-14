1f0f130436fa3d4e8cb306e689487e86f4f88c4a
rains <1782832653@qq.com>
2018-12-14 18:07:18 +0800

    Update main.js

--- src/web/main.js
+++ src/web/main.js
@@ -4,10 +4,12 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import echarts from 'echarts'
 Vue.use(ElementUI)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
+Vue.prototype.$echarts = echarts;
 
 /* eslint-disable no-new */
 new Vue({
