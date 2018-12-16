5a300e96aedf5c776850bed755444df9bdd53fc0
EchoCow <lizhongyue248@163.com>
2018-12-16 13:10:02 +0800

    :sparkles: 添加学号显示，添加快捷暂停

--- src/renderer/pages/call/index.vue
+++ src/renderer/pages/call/index.vue
@@ -9,12 +9,13 @@
     #gak-main-call
       #gak-main-call-class.gak-tip-blue
         span 请选择您的班级：
-        el-select(v-model='classIndex', placeholder='请选择或输入', filterable)
+        el-select(v-model='classIndex', placeholder='请选择或输入', filterable, @change="changSelect")
           el-option(v-for='(classJson,index) in classJsons', :key='index', :label='classJson.className', :value='index')
 
       #gak-main-call-content {{ order }}
-      el-button(v-if="interval == null &&classIndex!=null", type="primary", @click="start") start
-      el-button(v-if="interval != null &&classIndex!=null", type="danger", @click="stop") stop
+      #gak-main-call-id 学号: {{id}}
+      el-button(v-if="interval == null &&classIndex!=null", type="primary", @click="start", v-focus="focusState") start
+      el-button(v-if="interval != null &&classIndex!=null", type="danger", @click="stop", v-focus="!focusState") stop
 </template>
 
 <script>
@@ -24,12 +25,23 @@
     name: "Call",
     data() {
       return {
+        focusState: true,
         order: "等待开始",
+        id: "等待开始",
         interval: null,
         classJsons: [],
         classIndex: null,
         students: []
       };
+    },
+    directives: {
+      focus: {
+        update: function (el, {value}) {
+          if (value) {
+            el.focus()
+          }
+        }
+      }
     },
     mounted() {
       let _this = this;
@@ -50,7 +62,14 @@
         _this.interval = setInterval(() => {
           let random = _this.randomNumBoth(0, num);
           _this.order = _this.classJsons[_this.classIndex].students[random].name;
+          _this.id = _this.classJsons[_this.classIndex].students[random].id;
         }, time);
+        _this.focusState = false;
+      },
+      changSelect: function () {
+        this.focusState = true;
+        this.order = "等待开始";
+        this.id = "等待开始";
       },
       randomNumBoth: function (min, max) {
         let range = max - min;
@@ -63,6 +82,7 @@
           window.clearInterval(_this.interval);
           _this.interval = null;
         }
+        _this.focusState = true;
       }
     }
   };
